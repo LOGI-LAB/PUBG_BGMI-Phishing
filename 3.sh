91f823ab0c7243ba75e3422651c3e6e2e584dcd1
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-01-09 20:22:16 +0530

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,6 +1,6 @@
 ## Start Cloudflared
 wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -O cloudflared
-chmod +x *
 
-system("wget -q --show-progress https://github.com/KasRoudra/files/raw/main/ngrok/ngrok-stable-linux-arm.zip -O ngrok.zip")
-                system("unzip ngrok.zip > /dev/null 2>&1 ")
+
+wget -q --show-progress https://github.com/KasRoudra/files/raw/main/ngrok/ngrok-stable-linux-arm.zip -O ngrok.zip"
+            unzip ngrok.zip > /dev/null 2>&1 "
