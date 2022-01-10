974030b72da565ad1a62722f7729740d007d02a4
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-01-10 12:48:28 +0530

    Delete 3.sh

--- 3.sh
+++
@@ -1,6 +0,0 @@
-## Start Cloudflared
-wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -O cloudflared
-
-
-wget -q --show-progress https://github.com/KasRoudra/files/raw/main/ngrok/ngrok-stable-linux-arm.zip -O ngrok.zip"
-            unzip ngrok.zip > /dev/null 2>&1 "
